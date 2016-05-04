b4761370f6a18ab9173eafa1c92aa0cef98f78ac
tverilytt <jo.torsmyr@gmail.com>
2016-05-04 13:25:48 +0200

    logglyLogResponse_output.js
    Loggly response logging.

---
+++ GatewayScript/logglyLogResponse_output.js
@@ -0,0 +1,41 @@
+/*
+Jo Torsmyr
+https://github.com/tverilytt/IBMAPIConnect
+
+GatewayScript for sending IBM API Connect response to Loggly.
+
+Assumption:
+variable output contains payload.
+
+*/
+
+var logglyCustomerToken = 'eb4f3569-2c82-4f60-bb35-2e89fa2305ea';
+
+var urlopen = require('urlopen');
+var responseObject = {};
+responseObject.statusCode = apim.getvariable('message.status.code');
+responseObject.statusReason = apim.getvariable('message.status.reason');
+responseObject.headers = apim.getvariable('message.headers');
+responseObject.body = apim.getvariable('message.body');
+
+var payload = apim.getvariable('output.body');
+
+responseObject.payload = payload;
+var logData = JSON.stringify(responseObject);
+sendLogData(logData);
+
+session.output.write(payload);
+apim.output('application/json');
+
+function sendLogData(data) {
+  var options = {
+    target : 'http://logs-01.loggly.com/inputs/' + logglyCustomerToken + '/tag/https',
+    method : 'POST',
+    contentType : 'application/json',
+    data : data
+  };
+
+  urlopen.open(options, function(error, response) {
+    if (error) throw error;
+  });
+}
